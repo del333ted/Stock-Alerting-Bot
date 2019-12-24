82ced09e0fc59bc3b21ff8d2d7311b912349d4e0
Del333ted <del333ted@protonmail.com>
2019-12-25 02:31:19 +0300

    /story command

--- bot.js
+++ bot.js
@@ -17,33 +17,99 @@ bot.use(async (ctx, next) => {
 })
 
 bot.start(async (ctx, next) => {
-  ctx.replyWithHTML('Привет, я дополню твою историю. Нужно лишь написать её начало из нескольких предложений. Чем четче будет сформулировано начало, тем лучше будет результат.\n\n<b>GitHub бота:</b> github.com/LyoSU/history-ai-bot\n\nБот это лишь "обертка" для взаимодействия с API. Авторство принадлежит оригинальному автору проекта и все благодарности необходимо отправлять ему.\n\nGitHub проекта: github.com/mgrankin/ru_transformers\nВеб-версия проекта: text.skynet.center\n\n<b>Блог разработчика:</b> @LyBlog')
+  ctx.replyWithHTML(
+    'Привет, я дополню твою историю. Нужно лишь написать её начало из нескольких предложений. Чем четче будет сформулировано начало, тем лучше будет результат.\n\n/story <i>text</i> — команда для использования бота в чате.\n\n<b>GitHub бота:</b> github.com/del333ted/AI-Stories\n\nБот это лишь "обертка" для взаимодействия с API. Авторство принадлежит оригинальному автору проекта и все благодарности необходимо отправлять ему.\n\nGitHub проекта: github.com/mgrankin/ru_transformers\nВеб-версия проекта: text.skynet.center\n\n<b>Автор:</b> @del333ted',
+  )
+})
+
+bot.command('help', async (ctx, next) => {
+  ctx.replyWithHTML(
+    'Привет, я дополню твою историю. Нужно лишь написать её начало из нескольких предложений. Чем четче будет сформулировано начало, тем лучше будет результат.\n\n/story <i>text</i> — команда для использования бота в чате.\n\n<b>GitHub бота:</b> github.com/del333ted/AI-Stories\n\nБот это лишь "обертка" для взаимодействия с API. Авторство принадлежит оригинальному автору проекта и все благодарности необходимо отправлять ему.\n\nGitHub проекта: github.com/mgrankin/ru_transformers\nВеб-версия проекта: text.skynet.center\n\n<b>Автор:</b> @del333ted',
+  )
+})
+
+function commandArgument(text) {
+  const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i
+  const parts = regex.exec(text.trim())
+  return parts[3] || false
+}
+
+bot.command('story', async (ctx) => {
+  if (!ctx.chat || !(ctx.chat.type === 'group' || ctx.chat.type === 'supergroup')) {
+    return ctx.reply('Используйте эту команду в группе.')
+  }
+  const text = commandArgument(ctx.message.text)
+  if (text) {
+    const extend = got.extend({
+      responseType: 'json',
+      timeout: 10000,
+      throwHttpErrors: false,
+    })
+  
+    if (ctx.message.text) {
+      ctx.replyWithChatAction('typing')
+      const text = ctx.message.text
+  
+      const mediumResult = await extend.post(
+        'https://models.dobro.ai/gpt2/medium/',
+        {
+          json: {
+            prompt: text,
+            length: 60,
+            num_samples: 1,
+          },
+        },
+      )
+  
+      if (
+        mediumResult.body &&
+        mediumResult.body.replies &&
+        mediumResult.body.replies.length > 0
+      ) {
+        const result = `<i>${text}</i>${mediumResult.body.replies[0]}`
+  
+        ctx.replyWithHTML(result, {
+          reply_to_message_id: ctx.message.message_id,
+        })
+      }
+    }
+  }
 })
 
 bot.use(async (ctx, next) => {
+  if (!ctx.chat || ctx.chat.type === 'private') {
+    return
+  }
   const extend = got.extend({
     responseType: 'json',
     timeout: 10000,
-    throwHttpErrors: false
+    throwHttpErrors: false,
   })
 
   if (ctx.message.text) {
     ctx.replyWithChatAction('typing')
     const text = ctx.message.text
 
-    const mediumResult = await extend.post('https://models.dobro.ai/gpt2/medium/', {
-      json: {
-        prompt: text,
-        length: 60,
-        num_samples: 1
-      }
-    })
+    const mediumResult = await extend.post(
+      'https://models.dobro.ai/gpt2/medium/',
+      {
+        json: {
+          prompt: text,
+          length: 60,
+          num_samples: 1,
+        },
+      },
+    )
 
-    if (mediumResult.body && mediumResult.body.replies && mediumResult.body.replies.length > 0) {
+    if (
+      mediumResult.body &&
+      mediumResult.body.replies &&
+      mediumResult.body.replies.length > 0
+    ) {
       const result = `<i>${text}</i>${mediumResult.body.replies[0]}`
 
       ctx.replyWithHTML(result, {
-        reply_to_message_id: ctx.message.message_id
+        reply_to_message_id: ctx.message.message_id,
       })
     }
   }
