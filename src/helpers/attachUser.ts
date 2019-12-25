7025dab01e089440751325b5e8fdedd6d3052b9b
Del333ted <del333ted@protonmail.com>
2019-12-25 13:57:23 +0300

    lol!

--- src/helpers/attachUser.ts
+++ src/helpers/attachUser.ts
@@ -4,8 +4,8 @@ import { ContextMessageUpdate } from 'telegraf'
 
 export async function attachUser(ctx: ContextMessageUpdate, next) {
   try {
-    if (ctx.chat.id === ctx.from.id) {
-      const user = await findUser(ctx.from.id)
+    if (ctx.chat.id === ctx.message.from.id) {
+      await findUser(ctx.from.id)
     } else {
       const chat = await findChat(ctx.chat.id)
       ctx.dbchat = chat
