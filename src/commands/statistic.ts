49e3110c27bfbac3deed8f8378a40f37ce22bbea
Del333ted <del333ted@protonmail.com>
2020-01-13 16:45:18 +0300

    f

--- src/commands/statistic.ts
+++ src/commands/statistic.ts
@@ -4,13 +4,22 @@ import { UserModel, RequestModel } from '../models'
 export async function sendStatistic(ctx: ContextMessageUpdate) {
   if (ctx.message.from.id === Number(process.env.BOT_OWNER)) {
     const users = await UserModel.find({}).count()
-    const usersNotify = await UserModel.find({'settings.notify': true}).count()
+    const usersNotify = await UserModel.find({
+      'settings.notify': true,
+    }).count()
+
+    const usersDisabledSendout = await UserModel.find({
+      'settings.sendoutDisabled': true,
+    }).count()
+
     const requests = await RequestModel.find({}).count()
     return ctx.replyWithHTML(`<b>Статистика</b>
       
 Пользователей: <b>${users}</b>
 
 Пользователей с уведомлениями: <b>${usersNotify}</b>
+
+Пользователей с выключенной рассылкой: <b>${usersDisabledSendout}</b>
 
 Запросов: <b>${requests}</b>
         `)
